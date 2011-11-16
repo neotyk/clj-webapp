2910d907d055581cebfe4dd527b2d45042a70579
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-11-16 10:47:50 +0100

    Simplify initial data set.

--- src/clj_webapp/core.clj
+++ src/clj_webapp/core.clj
@@ -12,14 +12,11 @@
   []
   (dosync
    (alter *STORE* merge 
-          {"1" {:id "1"
-                :body "joker escaped arkham again"
+          {"1" {:body "joker escaped arkham again"
                 :isDone true}
-           "2" {:id "2"
-                :body "riddler sent riemann hypothesis"
+           "2" {:body "riddler sent riemann hypothesis"
                 :isDone false}
-           "3" {:id "3"
-                :body "bane wants to meet, not worried"
+           "3" {:body "bane wants to meet, not worried"
                 :isDone false}})))
 
 (defn read-todos!
